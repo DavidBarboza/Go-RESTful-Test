57b8bd5edcaccf536175334b93c225319d7c496c
davidbarboza <benjigoenitz@gmail.com>
2017-10-09 07:49:23 -0500

    Corrección en json

--- main.go
+++ main.go
@@ -62,11 +62,11 @@ var people []Person
 type Person struct {
 	ID string `json:"id,omitempty"`
 	Firstname string `json:"firstname,omitempty"`
-	Lastname string `json:lastname,omitempty`
-	Address *Address `json:address,omitempty`
+	Lastname string `json:"lastname,omitempty"`
+	Address *Address `json:"address,omitempty"`
 }
 
 type Address struct {
-	City string `json:city,omitempty`
-	State string `json:state,omitempty`
+	City string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
 }
